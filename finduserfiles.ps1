38d894df90b5e5324b3f3ceaebee9b0826319358
savefully <wertrew031@gmail.com>
2025-07-25 13:27:13 +0500

    Create finduserfiles.ps1

---
+++ finduserfiles.ps1
@@ -0,0 +1,24 @@
+param (
+    $Regex,
+    $Paths = 'Downloads;Downloads\Telegram Desktop'
+)
+$results = @()
+$Paths = $Paths -split ';'
+$users = Get-ChildItem -Path 'C:\Users' -Directory | Where-Object {
+    Test-Path "$($_.FullName)\Downloads"
+}
+foreach ($user in $users) {
+    foreach ($path in $Paths) {
+        $absolutePath = "$($user.FullName)\$path"
+        Get-ChildItem -Path $absolutePath -ErrorAction SilentlyContinue | Where-Object {
+            $_.Name -match $Regex
+        } | ForEach-Object {
+            $results += $_.FullName
+        }
+    }
+}
+$results = $results -join "`n"
+Write-Host "`nRegex: $Regex"
+Write-Host "`nPaths: $Paths"
+Write-Host "`nResults:`n"
+Write-Host $results
